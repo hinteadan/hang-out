80e527b502bb6ecbc4752e1c89cc373e6a9cfa3a
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-11-07 15:45:34 +0200

    Put default live reload port back

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function (grunt) {
                 port: 9899,
                 // Change this to '0.0.0.0' to access the server from outside.
                 hostname: 'localhost',
-                livereload: 35739
+                livereload: 35729
             },
             livereload: {
                 options: {
