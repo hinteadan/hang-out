a926647c747c7df7a6e65068670a9952c63e1b7c
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2014-11-11 02:00:27 +0200

    Some design for place suggestion

--- app/scripts/data/places.js
+++ app/scripts/data/places.js
@@ -12,10 +12,11 @@
       "location": null,
       "tags": [
         "tennis"
-      ]
+      ],
+      "logoUrl": "http://winners-tennis.ro/wp-content/themes/winners-tennis/images/logo_winners.gif"
   },
   {
-      "Activities": null,
+      "Activities": "Tennis",
       "name": "Club Transilvania",
       "address": "1 Decembrie 1918 nr. 41 Cluj Napoca",
       "details": "tel: 0730-690896/ 0264-409330",
@@ -25,8 +26,55 @@
           "lng": 23.549687
       },
       "tags": [
+        "tennis",
+        "football",
+        "tennis",
+        "basketball",
+        "voley"
+      ],
+      "logoUrl": "http://static.squarespace.com/static/540f12f4e4b0bda632e553fc/t/540ffb32e4b04939fb5be009/1414070626735/?format=400w"
+  },
+  {
+      "Activities": "Tennis",
+      "name": "Marc tenis",
+      "address": " Alea Baisoara 1/11, Cluj Napoca",
+      "details": "tel:  0744 478 769",
+      "websiteUrl": "http://www.marctenis.ro/",
+      "location": null,
+      "tags": [
         "tennis"
-      ]
+      ],
+      "logoUrl": null
+  },
+  {
+      "Activities": "tennis",
+      "name": "Tennis Cluj Scoala Nicolae Titulescu",
+      "address": "Aleea Herculane nr. 7 Cluj Napoca",
+      "details": "tel: 0749.268.619 sau 0726.912.317; în incinta Școlii Nicolae Titulescu",
+      "websiteUrl": "http://tenis.cluj.ro/",
+      "location": {
+          "lat": 46.770328,
+          "lng": 23.622025
+      },
+      "tags": [
+        "tennis"
+      ],
+      "logoUrl": null
+  },
+  {
+      "Activities": "tennis",
+      "name": "Enjoy",
+      "address": "  Alexandru Voievod",
+      "details": "tel: 0753488548",
+      "websiteUrl": "http://fiisportiv.ro/catalog/enjoy-terenuri-de-tenis-cluj-napoca",
+      "location": {
+          "lat": 46.771333,
+          "lng": 23.629668
+      },
+      "tags": [
+        "tennis"
+      ],
+      "logoUrl": null
   },
   {
       "Activities": "Table tennis",
@@ -40,132 +88,29 @@
       },
       "tags": [
         "Ping",
-        "Pong"
-      ]
-  },
-  {
-      "Activities": "Voleyball",
-      "name": "Club Transilvania",
-      "address": "1 Decembrie 1918 nr. 41 Cluj Napoca",
-      "details": "tel: 0730-690896 / 0264-409330",
-      "websiteUrl": "http://www.clubtransilvania.ro/",
-      "location": {
-          "lat": 46.774897,
-          "lng": 23.549687
-      },
-      "tags": [
-        "voley"
-      ]
-  },
-  {
-      "Activities": null,
-      "name": "Fabrica de sport",
-      "address": "Str. Tudor Vladimirescu, Cluj-Napoca (Taietura Turcului)",
-      "details": "tel: 0728-045623",
-      "websiteUrl": "http://fabricadesport.ro/",
-      "location": {
-          "lat": 46.782041,
-          "lng": 23.562281
-      },
-      "tags": []
-  },
-  {
-      "Activities": "Basketball",
-      "name": "Club Transilvania",
-      "address": "1 Decembrie 1918 nr. 41 Cluj Napoca",
-      "details": "tel: 0730-690896/ 0264-409330",
-      "websiteUrl": "http://www.clubtransilvania.ro/",
-      "location": {
-          "lat": 46.774897,
-          "lng": 23.549687
-      },
-      "tags": [
-        "basket"
-      ]
-  },
-  {
-      "Activities": null,
-      "name": "Fabrica de sport",
-      "address": "Str. Tudor Vladimirescu, Cluj-Napoca (Taietura Turcului)",
-      "details": "tel: 0728-045623",
-      "websiteUrl": "http://fabricadesport.ro/",
-      "location": {
-          "lat": 46.782041,
-          "lng": 23.562281
-      },
-      "tags": []
-  },
-  {
-      "Activities": "Soccer",
-      "name": "Club Transilvania",
-      "address": "1 Decembrie 1918 nr. 41 Cluj Napoca",
-      "details": "tel: 0730-690896/ 0264-409330",
-      "websiteUrl": "http://www.clubtransilvania.ro/",
-      "location": {
-          "lat": 46.774897,
-          "lng": 23.549687
-      },
-      "tags": [
-        "soccer",
-        "football"
-      ]
-  },
-  {
-      "Activities": null,
-      "name": "Fabrica de sport",
-      "address": "Str. Tudor Vladimirescu, Cluj-Napoca (Taietura Turcului)",
-      "details": "tel: 0728-045623",
-      "websiteUrl": "http://fabricadesport.ro/",
-      "location": {
-          "lat": 46.782041,
-          "lng": 23.562281
-      },
-      "tags": []
-  },
-  {
-      "Activities": "Football tennis",
-      "name": "Fabrica de sport",
-      "address": "Str. Tudor Vladimirescu, Cluj-Napoca (Taietura Turcului)",
-      "details": "tel: 0728-045623",
-      "websiteUrl": "http://fabricadesport.ro/",
-      "location": {
-          "lat": 46.782041,
-          "lng": 23.562281
-      },
-      "tags": []
-  },
-  {
-      "Activities": "Aerobics / Fitness / Gym",
-      "name": "Fabrica de sport",
-      "address": "Str. Tudor Vladimirescu, Cluj-Napoca (Taietura Turcului)",
-      "details": "tel: 0364-730051",
-      "websiteUrl": "http://fabricadesport.ro/",
-      "location": {
-          "lat": 46.782041,
-          "lng": 23.562281
-      },
-      "tags": [
-        "gym,",
+        "Pong",
+        "gym",
         "kangoo",
-        "jumps,",
-        "pilates,",
+        "jumps",
+        "pilates",
         "tae",
-        "bo,",
-        "step,",
-        "cardio,",
-        "yoga,",
-        "zumba,",
+        "bo",
+        "step",
+        "cardio",
+        "yoga",
+        "zumba",
         "belly",
-        "dance,",
-        "streching,",
+        "dance",
+        "streching",
         "body",
-        "training,",
-        "fitness,",
-        "bodybuilding,",
-        "kickboxing,",
-        "fitball,",
+        "training",
+        "fitness",
+        "bodybuilding",
+        "kickboxing",
+        "fitball",
         "aerobics"
-      ]
+      ],
+      "logoUrl": null
   },
   {
       "Activities": null,
@@ -176,7 +121,8 @@
       "location": null,
       "tags": [
         "fitness"
-      ]
+      ],
+      "logoUrl": null
   },
   {
       "Activities": null,
@@ -185,7 +131,8 @@
       "details": null,
       "websiteUrl": "http://www.BigFitness.ro",
       "location": null,
-      "tags": []
+      "tags": [],
+      "logoUrl": null
   }
     ]);
 
