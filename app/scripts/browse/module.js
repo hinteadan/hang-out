748994404a08b68831c855d1589d10571dabbfd0
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-11-11 13:28:10 +0200

    Scaffold Activity Details

--- app/scripts/browse/module.js
+++ app/scripts/browse/module.js
@@ -4,7 +4,8 @@
     angular.module('hang-out-browse', ['hang-out', 'hang-out-auth'])
     .config(['$routeProvider', function ($route) {
         $route
-            .when('/browse', { templateUrl: 'scripts/browse/browse.tmpl.html', controller: 'hangOutBrowseCtrl' });
+            .when('/browse', { templateUrl: 'scripts/browse/browse.tmpl.html', controller: 'hangOutBrowseCtrl' })
+            .when('/activity/:id', { templateUrl: 'scripts/browse/activity-details.tmpl.html', controller: 'hangOutActivityDetailsCtrl' });
     }]);
 
 }).call(this, this.angular);
