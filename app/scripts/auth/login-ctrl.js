f51897bc618a551383822dbf8d74a61cb184191f
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2014-11-23 20:02:15 +0200

    Redirect to browse if user is authenticated

--- app/scripts/auth/login-ctrl.js
+++ app/scripts/auth/login-ctrl.js
@@ -2,7 +2,13 @@
     'use strict';
 
     angular.module('hang-out-auth')
-    .controller('hangOutLogin', ['title', function (title) {
+    .controller('hangOutLogin', ['$location', 'title', 'hangOutAuth', function ($l, title, auth) {
+
+        if (auth.isAuthenticated()) {
+            $l.path('/browse');
+            return;
+        }
+
         title.set('Login');
     }]);
 
