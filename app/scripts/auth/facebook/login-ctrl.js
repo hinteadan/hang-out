861390f13c204375a70f64b7c77e2dbbdc7ecec7
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2014-11-23 21:41:51 +0200

    Fix Windows Phone Facebook Authentication

--- app/scripts/auth/facebook/login-ctrl.js
+++ app/scripts/auth/facebook/login-ctrl.js
@@ -2,25 +2,36 @@
     'use strict';
 
     angular.module('hang-out-auth-facebook')
-    .controller('hangOutFacebookLogin', ['$scope', '$location', '$facebook', 'model', 'hangOutAuth', function ($s, $l, $fb, m, auth) {
+    .controller('hangOutFacebookLogin', ['$scope', '$location', '$facebook', '$timeout', '$route', 'model', 'hangOutAuth', function ($s, $l, $fb, $t, $r, m, auth) {
 
-        $s.login = function () {
-            $fb.login().then(function () {
-                $fb.api("/me").then(function (response) {
-                    var user = new m.Individual(
+        function authenticateUser() {
+            $fb.api("/me").then(function (response) {
+                var user = new m.Individual(
                         response.first_name + ' ' + response.last_name,
                         response.email,
                         response.link)
                         .setAvatar('http://graph.facebook.com/' + response.id + '/picture?type=large');
 
-                    auth
-                        .login(user)
-                        .then(auth.authenticate)
-                        .then(function () {
-                            $l.path('/');
-                        });
-                });
+                auth
+                    .login(user)
+                    .then(auth.authenticate)
+                    .then(function () {
+                        $l.path('/');
+                    });
             });
+        }
+
+        function authenticateUserAndQueueAnother() {
+            if (auth.isAuthenticated()) {
+                return;
+            }
+            authenticateUser();
+            $t(authenticateUserAndQueueAnother, 1000);
+        }
+
+        $s.login = function () {
+            $fb.login().then(authenticateUser);
+            $t(authenticateUserAndQueueAnother, 1000);
         };
 
     }]);
