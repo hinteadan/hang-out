9c7ece92caeaf77705bbb803f564686ef5de9e15
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-11-24 14:49:23 +0200

    Fix: Suggestions when logging in (as we talked so many times before)

--- app/scripts/auth/mail/login-ctrl.js
+++ app/scripts/auth/mail/login-ctrl.js
@@ -1,16 +1,29 @@
-﻿(function (angular, notify) {
+﻿(function (angular, notify, store, JSON, _) {
     'use strict';
 
     angular.module('hang-out-auth-mail')
     .controller('hangOutMailLogin', ['$scope', '$location', 'hangOutMailAuth', 'model', function ($s, $l, auth, m) {
 
-        var me = {
-            email: null,
-            name: null
-        };
+        var storeKey = 'hang-out-user-emails',
+            userEmails = store[storeKey] ? JSON.parse(store[storeKey]) : [],
+            me = {
+                email: null,
+                name: null
+            };
+
+        function storeUserEmail() {
+            if (!me.email) {
+                return;
+            }
+            userEmails.push(me.email);
+            userEmails = _.uniq(userEmails, function (email) { return email.toLowerCase(); });
+            store[storeKey] = JSON.stringify(userEmails);
+        }
 
         $s.me = me;
+        $s.emails = function () { return userEmails; };
         $s.login = function () {
+            storeUserEmail();
             auth
             .login(new m.Individual(me.name, me.email))
             .then(function (isSuccess) {
@@ -22,4 +35,4 @@
         };
     }]);
 
-}).call(this, this.angular, this.alert);
+}).call(this, this.angular, this.alert, this.localStorage, this.JSON, this._);
