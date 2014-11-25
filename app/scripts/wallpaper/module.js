950c9c1670586e94184667c15c7564ede7ae4004
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2014-11-26 00:54:05 +0200

    Fix wallpapers and set some awesome defaults

--- app/scripts/wallpaper/module.js
+++ app/scripts/wallpaper/module.js
@@ -3,6 +3,17 @@
 
     angular.module('hang-out-wallpaper', ['ngAnimate'])
     .constant('wallpaper-change-event', 'hang-out-wallpaper-change')
-    .value('wallpaper-change-interval', 1000 * 60 * 10);//10 minutes
+    .value('wallpaper-default-images', [
+        'http://www.bhmpics.com/walls/happiness_beach_girl_2-other.jpg',
+        'http://www.magic4walls.com/wp-content/uploads/2013/12/mood-girl-woman-joy-happiness-positive-smile-hands-movement-leaves-leaves-sky-clouds-background-wallpaper.jpg',
+        'http://3.bp.blogspot.com/-NMkJyXrdi9c/UoRKUNalLiI/AAAAAAAAa8w/XR4RL1G_REE/s1600/finding-happiness.jpg',
+        'http://vividlife.me/ultimate/wp-content/uploads/2013/11/happiness-wallpapers_34664_1920x1200.jpg'
+    ])
+    .value('wallpaper-change-interval', 1000 * 60 * 10)//10 minutes
+    .run(['$rootScope', 'wallpaper', function ($root, wall) {
+        $root.$on('$routeChangeStart', function () {
+            wall.setDefault();
+        });
+    }]);
 
 }).call(this, this.angular);
--- app/scripts/wallpaper/wallpaper-srvc.js
+++ app/scripts/wallpaper/wallpaper-srvc.js
@@ -2,18 +2,22 @@
     'use strict';
 
     angular.module('hang-out-wallpaper')
-    .service('wallpaper', ['$rootScope', 'wallpaper-change-event', function ($root, changeEvent) {
+    .service('wallpaper', ['$rootScope', 'wallpaper-change-event', 'wallpaper-default-images', function ($root, changeEvent, defaultImages) {
 
         function setSingleWallpaperImage(imageUrl){
-            $root.$broadcast(changeEvent, [imageUrl, imageUrl]);
+            $root.$broadcast(changeEvent, imageUrl ? [imageUrl, imageUrl] : defaultImages);
         }
 
-        function setWallpaperImages(images) {
+        function setWallpaperImages(imageArray) {
+            var images = imageArray || defaultImages;
             $root.$broadcast(changeEvent, images.length === 1 ? [images[0], images[0]] : images);
         }
 
         this.setWallpaper = setSingleWallpaperImage;
         this.setWallpapers = setWallpaperImages;
+        this.setDefault = function () {
+            setWallpaperImages();
+        };
 
     }]);
 
