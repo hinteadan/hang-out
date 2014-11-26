a07298c6c12835209dde67107e95dd9db7ac1b45
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-11-26 12:56:32 +0200

    Update categories data

--- app/scripts/data/categories.js
+++ app/scripts/data/categories.js
@@ -5,33 +5,33 @@
     .constant('hangOutActivityCategories', [
   {
       "category": "indoor",
-      "image": "http://thumbs.dreamstime.com/x/cartoon-house-mascot-24058163.jpg",
-      "Other": null
+      "image": "http://us.cdn3.123rf.com/168nwm/deklofenak/deklofenak1205/deklofenak120500252/13709461-young-people-involved-in-sports.jpg",
+      "Other suggestions- cartoon type": "http://thumbs.dreamstime.com/x/cartoon-house-mascot-24058163.jpg"
   },
   {
       "category": "outdoor",
-      "image": "https://tsnra.files.wordpress.com/2012/03/hiking-family.jpg",
-      "Other": null
+      "image": "http://us.cdn4.123rf.com/168nwm/yuran-78/yuran-781301/yuran-78130100338/17414134-fun-couple-in-jump-on-the-outdoor-background.jpg",
+      "Other suggestions- cartoon type": "https://tsnra.files.wordpress.com/2012/03/hiking-family.jpg"
   },
   {
       "category": "water",
-      "image": "http://thumbs.dreamstime.com/x/water-fun-3038931.jpg",
-      "Other": null
+      "image": "http://www.coffscoast.com.au/sites/coffscoast.com.au/files/hero_shot_sup_for_print_0.jpg",
+      "Other suggestions- cartoon type": "http://thumbs.dreamstime.com/x/water-fun-3038931.jpg"
   },
   {
       "category": "mountain",
-      "image": "http://thumbs.dreamstime.com/x/mountain-bikers-mountain-vector-illustration-41044485.jpg",
-      "Other": null
+      "image": "https://tsnra.files.wordpress.com/2012/03/hiking-family.jpg",
+      "Other suggestions- cartoon type": "http://thumbs.dreamstime.com/x/mountain-bikers-mountain-vector-illustration-41044485.jpg"
   },
   {
       "category": "ballgames",
-      "image": "http://thumbs.dreamstime.com/x/ball-games-illustration-people-playing-various-sports-40495215.jpg",
-      "Other": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA0ANR0xfruCV4IN18RkBEHl5wwFmFUTuOCPUVG570Zf6lnTrW"
+      "image": "http://us.cdn1.123rf.com/168nwm/gekaskr/gekaskr1205/gekaskr120500064/13820239-soccer-ball-in-the-net-gate-on-the-football-field.jpg",
+      "Other suggestions- cartoon type": "http://thumbs.dreamstime.com/x/ball-games-illustration-people-playing-various-sports-40495215.jpg"
   },
   {
       "category": "social",
-      "image": "http://thumbs.dreamstime.com/x/people-playing-cards-vector-illustration-group-young-together-43237253.jpg",
-      "Other": null
+      "image": "http://www.popmythology.com/wp-content/uploads/2014/01/watching-video-games.jpg",
+      "Other suggestions- cartoon type": "http://thumbs.dreamstime.com/x/people-playing-cards-vector-illustration-group-young-together-43237253.jpg"
   }
     ]);
 
