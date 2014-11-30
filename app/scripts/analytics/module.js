32f541ceaca80047a394ce5ccb92f05c79c42461
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2014-11-30 21:02:58 +0200

    OD: Configure Analytics and Webmaster Tools

---
+++ app/scripts/analytics/module.js
@@ -0,0 +1,12 @@
+﻿(function (angular, localStore) {
+    'use strict';
+
+    angular.module('hang-out-analytics', ['angularytics'])
+        .config(['AngularyticsProvider', function (AngularyticsProvider) {
+            AngularyticsProvider.setEventHandlers(['Console', 'GoogleUniversal']);
+        }])
+        .run(['Angularytics', function (Angularytics) {
+            Angularytics.init();
+        }]);
+
+}).call(this, this.angular, this.localStorage);
