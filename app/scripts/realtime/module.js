244cf32e2d7dab60407f07d0439129dfe1c00eff
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-12-17 15:14:24 +0200

    First working version of SignalR scenario

--- app/scripts/realtime/module.js
+++ app/scripts/realtime/module.js
@@ -1,23 +1,57 @@
-﻿(function (angular) {
+﻿(function (angular, $) {
     'use strict';
 
-    angular.module('hang-out-real-time', ['SignalR'])
-    .controller('realTimeTestCtrl', ['$scope', 'Hub', function ($s, Hub) {
-        var entityHub = new Hub('entityHub', {
-            listeners: {
-                pong: function (timestamp) {
-                    console.log(timestamp);
-                }
-            },
-            methods: ['ping'],
-            errorHandler: function (error) {
-                console.error(error);
-            },
-            rootPath: 'http://localhost/HttpDataStore'
-        });
+    angular.module('hang-out-real-time', ['hang-out'])
+    .constant('realTimeRootPath', 'realtime')
+    .run(['storeUrl', 'realTimeRootPath', function (storeUrl, rootPath) {
+        $('body').append('<script src="' + storeUrl + rootPath + '/hubs" type="text/javascript"></script>');
+    }])
+
+    .service('hangOutRealtime', ['$timeout', 'storeUrl', 'realTimeRootPath', function ($t, storeUrl, rootPath) {
+
+        var realtime = null,
+            retryCount = 0,
+            retryMax = 10,
+            retryIn = 500;
+
+
+        function areHubsLoaded() {
+            return Boolean($.connection.entityHub);
+        }
+
+        function initialize() {
+            realtime = $.connection.entityHub;
+
+            realtime.client.pong = function (timestamp) {
+                console.log('server pong');
+                console.log(timestamp);
+            };
+
+            $.connection.hub.url = storeUrl + rootPath;
+            $.connection.hub.start().done(function () {
+                console.log('$.connection.hub.start().done');
+                realtime.server.ping();
+            });
+        }
+
+        function tryInitialize() {
+            if (areHubsLoaded()) {
+                initialize();
+                return;
+            }
+            else if (retryCount === retryMax) {
+                return;
+            }
+            retryCount++;
+            $t(tryInitialize, retryIn);
+        }
+        tryInitialize();
+    }])
+
+    .controller('realTimeTestCtrl', ['$scope', 'hangOutRealtime', function ($s, realtime) {
         $s.ping = function () {
-            entityHub.ping();
+            realtime
         };
     }]);
 
-}).call(this, this.angular);
+}).call(this, this.angular, this.jQuery);
