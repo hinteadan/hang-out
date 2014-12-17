32a948543fe405205bd3e8c38e0f4361ad3cc0db
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2014-12-17 11:22:09 +0200

    Scaffold basic realtime framework for SignlaR

---
+++ app/scripts/realtime/module.js
@@ -0,0 +1,23 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('hang-out-real-time', ['SignalR'])
+    .controller('realTimeTestCtrl', ['$scope', 'Hub', function ($s, Hub) {
+        var entityHub = new Hub('entityHub', {
+            listeners: {
+                pong: function (timestamp) {
+                    console.log(timestamp);
+                }
+            },
+            methods: ['ping'],
+            errorHandler: function (error) {
+                console.error(error);
+            },
+            rootPath: 'http://localhost/HttpDataStore'
+        });
+        $s.ping = function () {
+            entityHub.ping();
+        };
+    }]);
+
+}).call(this, this.angular);
