98bc9f26c598963c29acbd4da567591fc5d52fbe
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-12-23 16:02:09 +0200

    Update bower packs and fix build

--- app/scripts/external/config.js
+++ app/scripts/external/config.js
@@ -1,17 +1,3 @@
-﻿(function (addthisevent) {
-    'use strict';
-
-    addthisevent.settings({
-        //mouse: false,
-        css: false,
-        //outlook: { show: true, text: "Outlook Calendar" },
-        //google: { show: true, text: "Google Calendar" },
-        //yahoo: { show: true, text: "Yahoo Calendar" },
-        //hotmail: { show: true, text: "Hotmail Calendar" },
-        //ical: { show: true, text: "iCal Calendar" },
-        //facebook: { show: true, text: "Facebook Event" },
-        //dropdown: { order: "outlook,google,ical" },
-        //callback: ""
-    });
-
-}).call(this, this.addthisevent);
+﻿addthisevent.settings({
+    css: false
+});
