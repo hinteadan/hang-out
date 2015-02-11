c8b9ae6745d5683e5d4b2b16d0b8e18e5897d7a1
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-02-11 17:59:55 +0200

    Sample for entity validation

--- app/scripts/registration/register-ctrl.js
+++ app/scripts/registration/register-ctrl.js
@@ -1,9 +1,14 @@
-(function(angular){
+(function(angular, hds){
     'use strict';
 
     angular.module('hang-out-registration')
     .controller('hangOutRegisterCtrl', [function(){
 
+        var validator = new hds.Validation();
+        validator.QueueForValidation(new hds.Entity({ id: 'test-test-testicles' }), 'validatorTest', '123qweqwe').then(function(result){
+           validator.Validate(result.data, '123qweqwe').then(function(){});
+        });
+
     }]);
 
-}).call(this, this.angular);
+}).call(this, this.angular, this.H.DataStore);
