c9a13906e9937368cdcd738664b250e28fcf80cb
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-02-12 11:26:27 +0200

    Create model and screen for the registration form

--- app/scripts/registration/register-ctrl.js
+++ app/scripts/registration/register-ctrl.js
@@ -1,14 +1,41 @@
-(function(angular, hds){
+(function (angular, hds) {
     'use strict';
 
+    function RegistrationForm() {
+        
+        var self = this;
+
+        this.name = null;
+        this.email = null;
+        this.emailConfirm = null;
+        this.password = null;
+        this.passwordConfirm = null;
+
+        function isMinimallyFilled() {
+            return self.name && self.email && self.emailConfirm && self.password && self.passwordConfirm;
+        }
+
+        function isConfirmed() {
+            return self.email === self.emailConfirm && self.password === self.passwordConfirm;
+
+        }
+
+        this.isFilledOk = function () {
+            return isMinimallyFilled() && isConfirmed();
+        };
+    }
+
     angular.module('hang-out-registration')
-    .controller('hangOutRegisterCtrl', [function(){
+      .controller('hangOutRegisterCtrl', ['$scope', function ($s) {
 
-        var validator = new hds.Validation();
-        validator.QueueForValidation(new hds.Entity({ id: 'test-test-testicles' }), 'validatorTest', '123qweqwe').then(function(result){
-           validator.Validate(result.data, '123qweqwe').then(function(){});
-        });
+          //        var validator = new hds.Validation();
+          //        validator.QueueForValidation(new hds.Entity({ id: 'test-test-testicles' }), 'validatorTest', '123qweqwe').then(function(result){
+          //           validator.Validate(result.data, '123qweqwe').then(function(){});
+          //        });
 
-    }]);
+          $s.registration = new RegistrationForm();
+
+
+      }]);
 
 }).call(this, this.angular, this.H.DataStore);
