8b21b551297eac678c2b2f9eb018b3403d1248df
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-02-12 15:07:33 +0200

    OD: Registration process

--- app/scripts/registration/module.js
+++ app/scripts/registration/module.js
@@ -1,7 +1,7 @@
 (function (angular, localStore) {
     'use strict';
 
-    angular.module('hang-out-registration', ['ngRoute', 'hang-out', 'hang-out-analytics'])
+    angular.module('hang-out-registration', ['ngRoute', 'hang-out', 'hang-out-auth', 'hang-out-analytics'])
     .config(['$routeProvider', function ($route) {
         $route
             .when('/register', { templateUrl: 'scripts/registration/register.tmpl.html', controller: 'hangOutRegisterCtrl', isPublic: true });
--- app/scripts/registration/register-ctrl.js
+++ app/scripts/registration/register-ctrl.js
@@ -2,7 +2,7 @@
     'use strict';
 
     function RegistrationForm() {
-        
+
         var self = this;
 
         this.name = null;
@@ -26,15 +26,66 @@
     }
 
     angular.module('hang-out-registration')
-      .controller('hangOutRegisterCtrl', ['$scope', function ($s) {
+      .controller('hangOutRegisterCtrl', ['$scope', '$q', 'dataStore', 'model', 'hasher', function ($s, $q, provide, m, hasher) {
 
           //        var validator = new hds.Validation();
           //        validator.QueueForValidation(new hds.Entity({ id: 'test-test-testicles' }), 'validatorTest', '123qweqwe').then(function(result){
           //           validator.Validate(result.data, '123qweqwe').then(function(){});
           //        });
 
-          $s.registration = new RegistrationForm();
+          function createIndividiual() {
+              return new m.Individual(
+                  $s.registration.name,
+                  $s.registration.email,
+                  $s.registration.website
+                  );
+          }
 
+          $s.registration = new RegistrationForm();
+          $s.$watch('registration.email', function () {
+              $s.flag.isEmailTaken = false;
+          });
+
+          $s.isEmailAvailable = function () {
+              var deff = $q.defer();
+
+              provide.userViaEmail($s.registration.email).then(function (user) {
+                  var isFree = !Boolean(user);
+                  if (isFree) {
+                      deff.resolve($s.registration.email);
+                  }
+                  else {
+                      deff.reject(user);
+                  }
+              });
+
+              return deff.promise;
+          };
+
+          $s.applyRegistration = function () {
+              $s.flag.registrationError = null;
+              $s.flag.registrationSuccess = null;
+              $s.isEmailAvailable().then(
+                  function () {
+                      var clientId = hasher.hash($s.registration.email);
+                      provide.queueRegistration(createIndividiual(), clientId).then(function (registrationToken) {
+                          //Send reg. email
+                          $s.flag.registrationSuccess = 'You have successfully registered. Please check your Inbox or Spam folder to finalize the process.';
+                      },
+                      function (reason) {
+                          $s.flag.registrationError = reason;
+                      });
+                  },
+                  function () {
+                      $s.flag.isEmailTaken = true;
+                  });
+          };
+
+          $s.flag = {
+              isEmailTaken: false,
+              registrationError: null,
+              registrationSuccess: null,
+          };
 
       }]);
 
