543991d98577129793429c9191db6e053667d6af
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-02-12 17:45:01 +0200

    [Untested] - auth via user/pass

---
+++ app/scripts/auth/user/login-ctrl.js
@@ -0,0 +1,41 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('hang-out-auth-user')
+    .controller('hangOutUserLogin', ['$scope', '$q', '$location', 'model', 'hasher', 'hangOutAuth', 'dataStore', function ($s, $q, $l, m, hasher, auth, provide) {
+
+        function authenticateUserPass(username, password) {
+
+            var deff = $q.defer();
+
+            provide.userViaEmail(username).then(function (user) {
+                if (!user) {
+                    deff.reject('Invalid credentials');
+                    return;
+                }
+                deff.resolve(user);
+            });
+
+            return deff.promise;
+        }
+
+        function authenticate() {
+            authenticateUserPass($s.username, $s.password).then(function (user) {
+                auth
+                    .login(user)
+                    .then(auth.authenticate)
+                    .then(function () {
+                        $l.path('/');
+                    });
+            });
+        }
+
+        $s.username = null;
+        $s.password = null;
+        $s.login = function () {
+            authenticate();
+        };
+
+    }]);
+
+}).call(this, this.angular);
---
+++ app/scripts/auth/user/module.js
@@ -0,0 +1,6 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('hang-out-auth-user', ['hang-out', 'hang-out-auth']);
+
+}).call(this, this.angular);
