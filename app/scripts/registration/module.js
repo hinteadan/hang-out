3b6931eda7737da6ce27bf8d1408afe492608ec2
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-02-12 16:21:57 +0200

    Validate registration

--- app/scripts/registration/module.js
+++ app/scripts/registration/module.js
@@ -4,7 +4,8 @@
     angular.module('hang-out-registration', ['ngRoute', 'hang-out', 'hang-out-auth', 'hang-out-notify', 'hang-out-analytics'])
     .config(['$routeProvider', function ($route) {
         $route
-            .when('/register', { templateUrl: 'scripts/registration/register.tmpl.html', controller: 'hangOutRegisterCtrl', isPublic: true });
+            .when('/register', { templateUrl: 'scripts/registration/register.tmpl.html', controller: 'hangOutRegisterCtrl', isPublic: true })
+            .when('/validate-user/:clientId/:token', { templateUrl: 'scripts/registration/validate.tmpl.html', controller: 'hangOutValidateUserCtrl', isPublic: true });
     }]);
 
 }).call(this, this.angular, this.localStorage);
---
+++ app/scripts/registration/validate-ctrl.js
@@ -0,0 +1,25 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('hang-out-registration')
+      .controller('hangOutValidateUserCtrl', ['$scope', '$q', '$location', '$routeParams', 'dataStore', 'hangOutAuth', function ($s, $q, $l, $p, provide, auth) {
+
+          var clientId = $p.clientId,
+              validationToken = $p.token;
+
+          provide.validateRegistration(clientId, validationToken).then(function (user) {
+              
+              auth.login(user).then(function (key) {
+                  auth.authenticate(key).then(function () {
+                      $l.path('/');
+                      return true;
+                  });
+              });
+
+          }, function (reason) {
+              $s.error = reason;
+          });
+
+      }]);
+
+}).call(this, this.angular);
